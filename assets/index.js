795d2ef69a9d919bc1d292ef16ad6464e836634d
David Behar <davbelom@gmail.com>
2019-09-15 13:16:09 -0500

    Creacion de la clase MediaPlayer e implementacion de play/pause

---
+++ assets/index.js
@@ -0,0 +1,25 @@
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+
+function MediaPlayer(config) {
+  this.media = config.el;
+}
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+const player = new MediaPlayer({
+  el: video
+});
+
+button.onclick = () => player.togglePlay();
